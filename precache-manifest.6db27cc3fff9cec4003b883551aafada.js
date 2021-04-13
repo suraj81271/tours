22ac09df7711f0962ab93001815b66f69a94bbe0
Suraj <surajsingh81271@gmail.com>
2021-04-13 17:57:03 +0530

    Updates

---
+++ precache-manifest.6db27cc3fff9cec4003b883551aafada.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e9c9cff530fc5a4632a2dab41748366",
+    "url": "/tours/index.html"
+  },
+  {
+    "revision": "9957dcfebc54cec8b75e",
+    "url": "/tours/static/css/main.c3e53c0d.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/tours/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tours/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9957dcfebc54cec8b75e",
+    "url": "/tours/static/js/main.d7961a15.chunk.js"
+  },
+  {
+    "revision": "cabdc42b426759795a4b",
+    "url": "/tours/static/js/runtime-main.3b7494d8.js"
+  }
+]);
